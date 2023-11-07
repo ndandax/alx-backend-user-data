68722c52e780dcc199bd39ae9725d731f5e918a1
ndandax <sebandandala99@gmail.com>
2023-11-07 22:29:22 +0300

    authentication

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,10 +25,12 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> str:
     """unauthorized function"""
     abort(401)
+
 
 @app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> str:
