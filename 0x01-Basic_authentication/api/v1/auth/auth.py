bc5072d6a1011dffc965a14a7be3d0fd8987c870
ndandax <sebandandala99@gmail.com>
2023-11-07 17:06:56 +0300

    authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,18 @@ class Auth:
     """Authentication class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require authentication func"""
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path in excluded_paths:
+            return False
+        if not path.endswith('/'):
+            path += '/'
+        for i in excluded_paths:
+            if path.startswith(i):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization header"""
